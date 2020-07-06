02c56d5f13381c8ba2814fc01f1d3c9b6b3b2e00
mansi0111 <65336958+mansi0111@users.noreply.github.com>
2020-07-06 15:13:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+(function () {
+  const buttons = document.querySelectorAll(".counterBtn");
+  let count = 0;
+  const counter = document.querySelector("#counter");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (button.classList.contains("prevBtn")) {
+        count--;
+      } else if (button.classList.contains("nextBtn")) {
+        count++;
+      }
+      counter.textContent = count;
+      if (count < 0) {
+        counter.style.color = "red";
+      } else if (count > 0) {
+        counter.style.color = "green";
+      } else {
+        counter.style.color = "#333333";
+      }
+    });
+  });
+})();
